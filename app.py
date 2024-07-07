4010c7a0451adb01d8fcd92c7c514d310692ce62
MiguelCarreraE <90919223+MiguelCarreraE@users.noreply.github.com>
2024-07-06 22:08:17 -0600

    created date

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, jsonify
 import firebase_admin
 from firebase_admin import credentials, storage
-from datetime import timedelta
-
+from datetime import timedelta, datetime
 
 # Configurar Firebase
 cred = credentials.Certificate('esp32-fb6da-firebase-adminsdk-ko1ku-2923577eaa.json')
@@ -18,25 +17,27 @@ def list_images_inside():
     interior = []
     blobs = bucket.list_blobs(prefix='Interior/')
     for blob in blobs:
+        formatted_date = blob.time_created.strftime('%Y-%m-%d %H:%M:%S')
         interior.append({
             'name': blob.name,
-            'url': blob.generate_signed_url(timedelta(seconds=604800))
+            'url': blob.generate_signed_url(timedelta(seconds=604800)),
+            'created': formatted_date
         })
     return jsonify(interior)
-    
+
 @app.route('/exterior', methods=['GET'])
 def list_images_outside():
     bucket = storage.bucket()
     exterior = []
     blobs = bucket.list_blobs(prefix='Exterior/')
     for blob in blobs:
+        formatted_date = blob.time_created.strftime('%Y-%m-%d %H:%M:%S')
         exterior.append({
             'name': blob.name,
-            'url': blob.generate_signed_url(timedelta(seconds=604800))
+            'url': blob.generate_signed_url(timedelta(seconds=604800)),
+            'created': formatted_date
         })
     return jsonify(exterior)
-    
-
 
 if __name__ == '__main__':
     app.run(debug=True)
